b8d95284f148cc3884924659156204bcb48adc83
tjel <tomek.jelinski@gmail.com>
2016-11-07 04:44:27 +0100

    Add penta.cpp.

---
+++ penta.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+
+int main( ) 
+{ 
+    double a; 
+    cout<< "Enter a"; 
+    cin>>a;
+ 
+    cout<< "Perimeter of the pentagon is: "<< 5*a; 
+    cout<< "Area of the pentagon is: "<< ;
+    return 0;
+} 
