8756f3577b4115cbcea25149faaf418a8ed0d64c
jan-medrek <jan.medrek@smcebi.edu.pl>
2016-11-21 11:28:51 +0100

    komentarze

--- penta.cpp
+++ penta.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main( ) 
 { 
+	//inicjalizacja zmiennych
     double a; 
+
+	//pobranie a
     cout<< "Enter a"; 
     cin>>a;
  
+	//obliczenia i wyjscie
     cout<< "Perimeter of the pentagon is: "<< 5*a; 
     cout<< "Area of the pentagon is:" << 5*a*a*sqrt(3)/4;
     return 0;
