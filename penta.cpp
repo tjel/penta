52a9faa6d1730e06527960100e38e1f7ec317731
Piotr Dec <Piotr Dec>
2016-11-21 11:35:46 +0100

    nice code

--- penta.cpp
+++ penta.cpp
@@ -1,15 +1,15 @@
 # include <iostream>
-#include <cmath>
+# include <cmath>
 
 using namespace std;
 
-int main( ) 
-{ 
-    double a; 
-    cout<< "Enter a"; 
+int main( )
+{
+    double a;
+    cout<< "Enter a";
     cin>>a;
- 
-    cout<< "Perimeter of the pentagon is: "<< 5*a; 
+
+    cout<< "Perimeter of the pentagon is: "<< 5*a;
     cout<< "Area of the pentagon is:" << 5*a*a*sqrt(3)/4;
     return 0;
-} 
+}
